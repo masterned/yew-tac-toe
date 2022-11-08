300b4f5d8f154cb1e0fa6e83ebf276960387a683
Spencer Dent <mr.spencerdent@gmail.com>
2022-11-08 17:23:08 -0500

    feat: emulate react tutorial starter code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,58 @@
 use yew::{function_component, html, Html};
 
 #[function_component]
-fn App() -> Html {
-    html! { "Hello World" }
+fn Square() -> Html {
+    html! {
+        <button class="square">
+        </button>
+    }
+}
+
+fn render_square(_i: i32) -> Html {
+    html! { <Square /> }
+}
+
+#[function_component]
+fn Board() -> Html {
+    let status = "Next player: X";
+
+    html! {
+        <div>
+            <div class="status">{status}</div>
+            <div class="board-row">
+                { render_square(0) }
+                { render_square(1) }
+                { render_square(2) }
+            </div>
+            <div class="board-row">
+                { render_square(3) }
+                { render_square(4) }
+                { render_square(5) }
+            </div>
+            <div class="board-row">
+                { render_square(6) }
+                { render_square(7) }
+                { render_square(8) }
+            </div>
+        </div>
+    }
+}
+
+#[function_component]
+fn Game() -> Html {
+    html! {
+        <div class="game">
+            <div class="game-board">
+                <Board />
+            </div>
+            <div class="game-info">
+                <div>{ "status" }</div>
+                <ol>{ "todo" }</ol>
+            </div>
+        </div>
+    }
 }
 
 fn main() {
-    yew::Renderer::<App>::new().render();
+    yew::Renderer::<Game>::new().render();
 }
